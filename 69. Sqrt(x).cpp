97e2f75eb8300a7f107e71a1407d8e9702108aa9
kent110655 <53744087+kent110655@users.noreply.github.com>
2021-03-10 04:16:57 +0800

    Update 69. Sqrt(x).cpp

--- 69. Sqrt(x).cpp
+++ 69. Sqrt(x).cpp
@@ -6,17 +6,17 @@ public:
         int left=0;
         int right=x;
         int mid;
-        while(left<right)
+        while(left<=right)
         {
             mid=left+(right-left)/2;
             if(mid == x/mid)
                 return mid;
             else if(mid > x/mid)
-                right=mid;
+                right=mid-1;
             else
                 left=mid+1;
         }
-        return left-1;
+        return right;
     }
 };
 
