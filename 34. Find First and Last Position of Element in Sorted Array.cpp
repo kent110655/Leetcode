2697223beaad438e02912c924ab635265092be6d
kent110655 <53744087+kent110655@users.noreply.github.com>
2021-03-10 15:05:15 +0800

    Update 34. Find First and Last Position of Element in Sorted Array.cpp

--- 34. Find First and Last Position of Element in Sorted Array.cpp
+++ 34. Find First and Last Position of Element in Sorted Array.cpp
@@ -1,5 +1,58 @@
 class Solution {
 public:
+    int findFirstPosi(vector<int>& nums, int target)
+    {
+        //找最左端target
+        int size = nums.size();
+        int left=0;
+        int right=size-1;
+        int mid;
+        while(left<=right)
+        {
+            mid=left+(right-left)/2;
+            if(nums[mid]>=target)    //取左 or target
+            {
+                right=mid-1;
+            }
+            else    //取右
+            {
+                left=mid+1;
+            }
+        }
+        return right+1;
+    }
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        int size=nums.size();
+        if(size==0)
+        {
+            ans.push_back(-1);
+            ans.push_back(-1);
+            return ans;
+        }
+        
+        int first=findFirstPosi(nums,target);
+        int end=findFirstPosi(nums,target+1)-1;
+        if(first==size || nums[first]!=target)
+        {
+            ans.push_back(-1);
+            ans.push_back(-1);
+            return ans;
+        }
+        else
+        {
+            ans.push_back(first);
+            ans.push_back(end);
+            return ans;
+        }
+    }
+};
+
+/*
+algo 1:
+class Solution {
+public:
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int> ans;
         int size=nums.size();
@@ -56,9 +109,69 @@ public:
     }
 };
 
-/*
 Comment:
 Binary search
 題：求得target的最左端跟最右端位置。
 利用兩種binary search分別求得兩端位置。
+*/
+
+/*
+algo 2:
+class Solution {
+public:
+    int findFirstPosi(vector<int>& nums, int target)
+    {
+        //找最左端target
+        int size = nums.size();
+        int left=0;
+        int right=size-1;
+        int mid;
+        while(left<=right)
+        {
+            mid=left+(right-left)/2;
+            if(nums[mid]>=target)    //取左 or target
+            {
+                right=mid-1;
+            }
+            else    //取右
+            {
+                left=mid+1;
+            }
+        }
+        return right+1;
+    }
+    
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> ans;
+        int size=nums.size();
+        if(size==0)
+        {
+            ans.push_back(-1);
+            ans.push_back(-1);
+            return ans;
+        }
+        
+        int first=findFirstPosi(nums,target);
+        int end=findFirstPosi(nums,target+1)-1;
+        if(first==size || nums[first]!=target)
+        {
+            ans.push_back(-1);
+            ans.push_back(-1);
+            return ans;
+        }
+        else
+        {
+            ans.push_back(first);
+            ans.push_back(end);
+            return ans;
+        }
+        
+    }
+};
+
+Comment:
+優化algo 1，不用兩個binary search
+只寫一個取最左端的binary search
+最左端位置：尋找target
+最右端位置：尋找target+1的最左端，得出的位置再-1即可
 */
