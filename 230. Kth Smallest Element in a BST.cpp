6dbb10ca4ad2347d0ecd94826c953b30970f3a5d
kent110655 <53744087+kent110655@users.noreply.github.com>
2021-04-15 23:12:03 +0800

    Update 230. Kth Smallest Element in a BST.cpp

--- 230. Kth Smallest Element in a BST.cpp
+++ 230. Kth Smallest Element in a BST.cpp
@@ -12,6 +12,34 @@
 class Solution {
 public:
     int kthSmallest(TreeNode* root, int k) {
+        int leftCount = countNode(root->left);
+        if(leftCount == k-1)
+            return root->val;
+        else if(leftCount > k-1)
+            return kthSmallest(root->left, k);
+        else
+            return kthSmallest(root->right, k - leftCount -1);
+    }
+private:
+    int countNode(TreeNode* root)
+    {
+        if(root == NULL)
+            return 0;
+        return 1 + countNode(root->left) + countNode(root->right);
+    }
+};
+
+/*
+Comment:
+利用Recursive實作。
+時間複雜度：Ｏ(n)
+空間複雜度：Ｏ(1)
+*/
+/*
+Algo 1:
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
         count = k;
         DFS(root);
         return ans;
@@ -33,7 +61,6 @@ private:
         DFS(root->right);
     }
 };
-/*
 Comment:
 利用DFS找Inorder，BST的Inorder即是大小排序。
 時間複雜度：Ｏ(n)
