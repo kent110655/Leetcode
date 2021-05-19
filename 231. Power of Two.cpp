ee924a9a7328dd9c6c9032146fa8e41e666061c9
kent110655 <53744087+kent110655@users.noreply.github.com>
2021-05-19 17:29:50 +0800

    Update 231. Power of Two.cpp

--- 231. Power of Two.cpp
+++ 231. Power of Two.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
+        return (n > 0) && (n & (n-1)) == 0;
+    }
+};
+/*
+Algo 2:
+利用 n & (n-1) 即是消除最低位的1的操作
+如果消除最低位1以後為0即代表只有一個1
+*/
+
+/*
+Algo 1:
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
         if(n == 0)
             return false;
         bool one = false;
@@ -15,7 +29,6 @@ public:
         return true;
     }
 };
-/*
 Comment:
 判斷使否為2的次方數
 判斷integer的二進位是否只有一個1即可
