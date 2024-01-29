f80c0f9e2173878874703a6b6888c431284943ff
kent110655 <53744087+kent110655@users.noreply.github.com>
2024-01-29 17:30:53 +0800

    Update 18. 4Sum.cpp

--- 18. 4Sum.cpp
+++ 18. 4Sum.cpp
@@ -2,6 +2,97 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         vector<vector<int>> result;
+        sort(nums.begin(), nums.end());
+        kSum(nums, target, 4, result);
+        return result;
+    }
+
+    void kSum(vector<int>& nums, long target, int k, vector<vector<int>>& result)
+    {
+        vector<int> cur;
+        dfs(nums, target, k, 0, nums.size()-1, cur, result);
+    }
+
+    void dfs(vector<int>& nums, long target, int k, int left, int right, vector<int> cur, vector<vector<int>>& result)
+    {
+        if(k>2)
+        {
+            for(int i = left; i <= right-k+1; i++)
+            {
+                if(i > left && nums[i] == nums[i-1])
+                {
+                    continue;
+                }
+                vector<int> next_cur(cur.begin(), cur.end());
+                next_cur.push_back(nums[i]);
+                dfs(nums, target-nums[i], k-1, i+1, right, next_cur, result);
+            }
+        }
+        else if(k==2)
+        {
+            while(left < right)
+            {
+                if(target == nums[left] + nums[right])
+                {
+                    vector<int> tempt(cur.begin(), cur.end());
+                    tempt.push_back(nums[left]);
+                    tempt.push_back(nums[right]);
+                    result.push_back(tempt);
+                    while(left < right && nums[left] == nums[left+1])
+                    {
+                        left++;
+                    }
+                    while(left < right && nums[right] == nums[right-1])
+                    {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
+                else if(target < nums[left] + nums[right])
+                {
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
+            }
+        }
+    }
+};
+
+/*
+Algo1: KSum (延伸通解)
+
+Time Complexity：O(nlogn + n^k)
+    sort: O(nlogn)
+    dfs: 主要是for loop -> O(n^k)
+
+Space Complexity：O(n+k)
+    result使用: O(n)
+    recursive深度: O(k)
+    recursive每層: O(1)
+
+
+Comment:
+2 pointer解法，與3Sum類似
+先由小到大sort一遍
+再來以兩個for迴圈指定此次要選定的前k個數
+最後兩個數再以two pointer方式動態調整到match
+為了加速，在過程中需進行剪枝，把一些不需要跑的loop提前結束
+例如重複的數字、剩餘數字數量不足...等等
+此例把4Sum的解法轉成KSum，並利用Recursive來實作指定前面k個數
+*/
+
+
+/*
+Algo 2:
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> result;
 
         sort(nums.begin(), nums.end());
         int size = nums.size();
@@ -51,7 +142,7 @@ public:
     }
 };
 
-/*
+
 Time Complexity：O(n^3)
 Space Complexity：O(1)
 Comment:
